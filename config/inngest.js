59d5a21beb2469dc004f2d388d6f4910f618e20b
houssein Elyatim <houssein.elyatim@ensi-uma.tn>
2026-04-04 22:04:16 +0100

    fixed som errors

--- config/inngest.js
+++ config/inngest.js
@@ -50,15 +50,16 @@ export const syncUserDeletation = inngest.createFunction(
         id: 'delete-user-with-clerk',
     },
     {
-        event : 'clerk/user.deleted',
+        event : 'clerk/user.deleted'
+    },
         async ({event})=>{
             const {id, first_name, last_name, email_addresses, image_url} = event.data
-    const userData= {
-        _id: id,
-        email: email_addresses[0].email_address,
-        name: first_name + " " + last_name,
-        imageUrl: image_url
-    }
+            const userData= {
+                _id: id,
+                email: email_addresses[0].email_address,
+                name: first_name + " " + last_name,
+                imageUrl: image_url
+            }
     await connectDB()
     await User.findByIdAndDelete(id)
 }
