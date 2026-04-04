7c58cc85dacfe8c9d05569b2193fa9db07483695
houssein Elyatim <houssein.elyatim@ensi-uma.tn>
2026-04-04 22:10:20 +0100

    fixed some errors

--- config/inngest.js
+++ config/inngest.js
@@ -52,16 +52,11 @@ export const syncUserDeletation = inngest.createFunction(
     {
         event : 'clerk/user.deleted'
     },
-        async ({event})=>{
-            const {id, first_name, last_name, email_addresses, image_url} = event.data
-            const userData= {
-                _id: id,
-                email: email_addresses[0].email_address,
-                name: first_name + " " + last_name,
-                imageUrl: image_url
-            }
-    await connectDB()
-    await User.findByIdAndDelete(id)
-}
+    async ({event})=> {
+        const {id} = event.data
+
+        await connectDB()
+        await User.findByIdAndDelete(id)
+
     }
 )
